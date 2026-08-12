c2b153e0991c938ead508ce8449ccc35bcffec51
Artemis Tosini <artemis.tosini@obsidian.systems>
2026-08-12 13:45:34 -0400

    Shell out to get unit graph

--- flake.nix
+++ flake.nix
@@ -1,31 +1,38 @@
 {
   inputs = {
     nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    { nixpkgs, ... }:
+    { nixpkgs, fenix, ... }:
     let
       inherit (nixpkgs) lib;
-      makePkgs = system: import nixpkgs { inherit system; };
+      makePkgs =
+        system:
+        import nixpkgs {
+          inherit system;
+          overlays = [ fenix.overlays.default ];
+        };
       forAllSystems = f: lib.genAttrs lib.systems.flakeExposed (system: f (makePkgs system));
     in
     {
       devShells = forAllSystems (pkgs: {
         default = pkgs.mkShell {
-          packages = with pkgs; [
-            rustc
-            cargo
-            rust-analyzer
-            clippy
-            rustfmt
-
-            pkg-config
-
-            openssl
-            curl
-            libgit2
-            libssh2
+          packages = [
+            # Use unstable cargo to be safe.
+            # Technically we could use __CARGO_TEST_CHANNEL_OVERRIDE_DO_NOT_USE_THIS=nightly instead
+            (pkgs.fenix.complete.withComponents [
+              "cargo"
+              "clippy"
+              "rust-src"
+              "rustc"
+              "rustfmt"
+            ])
+            pkgs.rust-analyzer-nightly
           ];
         };
       });
--- src/main.rs
+++ src/main.rs
@@ -1,113 +1,99 @@
-use std::collections::HashSet;
+use std::{path::PathBuf, process::Stdio};
 
-use anyhow::Context as _;
-use cargo::{
-    core::{
-        SourceKind::Path,
-        compiler::{
-            Unit, UnitInterner, UserIntent,
-            unit_graph::{self, UnitGraph},
-        },
-    },
-    ops::create_bcx,
-    util::command_prelude::{ArgMatchesExt, Command, CommandExt as _, ProfileChecking},
-};
+use color_eyre::eyre::{self, WrapErr as _};
 
-// It seems like cargo doesn't have an existing function for this?
-fn order_units<'a>(
-    known_units: &mut HashSet<&Unit>,
-    ordered_units: &mut Vec<&'a Unit>,
-    unit_graph: &'a UnitGraph,
-    root_unit: &Unit,
-) {
-    let deps = unit_graph
-        .get(root_unit)
-        .map(|x| x.as_slice())
-        .unwrap_or_default();
-
-    for dep in deps {
-        if !known_units.contains(&dep.unit) {
-            ordered_units.push(&dep.unit);
-        }
-    }
+#[derive(Debug, serde::Deserialize)]
+#[serde(rename_all = "kebab-case")]
+enum CompileMode {
+    Test,
+    Build,
+    Check,
+    Doc,
+    Dooctest,
+    Docscrape,
+    RunCustomBuild,
 }
 
-fn main() -> anyhow::Result<()> {
-    // rustc things.
-    // Could be replaced with a `cargo build --unit-graph`
-    // by either using nightly cargo or waiting until it is stabilised:
-    // https://github.com/rust-lang/cargo/issues/8002
+#[derive(Debug, serde::Deserialize)]
+struct Target {
+    pub kind: Vec<String>,
+    pub crate_types: Vec<String>,
+    pub name: String,
+    pub src_path: PathBuf,
+    pub edition: String,
+    pub doc: bool,
+    pub doctest: bool,
+    pub test: bool,
+}
 
-    let args = Command::new("cargo-dyndrv")
-        .about("Check a local package and all of its dependencies for errors")
-        .arg_future_incompat_report()
-        .arg_message_format()
-        .arg_silent_suggestion()
-        .arg_package_spec(
-            "Package to build (see `cargo help pkgid`)",
-            "Build all packages in the workspace",
-            "Exclude packages from the build",
-        )
-        .arg_targets_all(
-            "Build only this package's library",
-            "Build only the specified binary",
-            "Build all binaries",
-            "Build only the specified example",
-            "Build all examples",
-            "Build only the specified test target",
-            "Build all targets that have `test = true` set",
-            "Build only the specified bench target",
-            "Build all targets that have `bench = true` set",
-            "Build all targets",
-        )
-        .arg_features()
-        .arg_release("Build artifacts in release mode, with optimizations")
-        .arg_redundant_default_mode("debug", "build", "release")
-        .arg_profile("Build artifacts with the specified profile")
-        .arg_parallel()
-        .arg_target_triple("Build for the target triple")
-        .arg_target_dir()
-        .arg_artifact_dir()
-        .arg_timings()
-        .arg_compile_time_deps()
-        .arg_manifest_path()
-        .arg_ignore_rust_version()
-        .get_matches();
+#[derive(Debug, serde::Deserialize)]
+struct Profile {
+    pub name: String,
+    pub opt_level: String,
+    pub lto: String,
+    pub codegen_backend: Option<String>,
+    pub codgen_units: Option<u32>,
+    pub debuginfo: u32,
+    pub split_debuginfo: Option<String>,
+    pub debug_assertions: bool,
+    pub overflow_checks: bool,
+    pub rpath: bool,
+    pub incremental: bool,
+    pub panic: String,
+    // TODO: Strip
+}
 
-    let gctx = cargo::util::GlobalContext::default().context("creating Cargo global context")?;
+#[derive(Debug, serde::Deserialize)]
+struct Dependency {
+    pub index: usize,
+    pub extern_crate_name: String,
+}
 
-    let ws = args
-        .workspace(&gctx)
-        .context("creating Cargo workspace object")?;
+#[derive(Debug, serde::Deserialize)]
+struct Unit {
+    pub pkg_id: String,
+    pub target: Target,
+    pub profile: Profile,
+    pub platform: Option<String>,
+    pub mode: CompileMode,
+    pub features: Vec<String>,
+    pub dependencies: Vec<Dependency>,
+}
 
-    let intent = match args.get_one::<String>("profile").map(String::as_str) {
-        Some("test") => UserIntent::Test,
-        Some("bench") => UserIntent::Bench,
-        Some("check") => UserIntent::Check { test: false },
-        _ => UserIntent::Build,
-    };
+#[derive(Debug, serde::Deserialize)]
+struct UnitGraph {
+    pub version: u32,
+    pub units: Vec<Unit>,
+    pub roots: Vec<usize>,
+}
 
-    let compile_opts = args
-        .compile_options(&gctx, intent, Some(&ws), ProfileChecking::Custom)
-        .context("creating compile options")?;
-    let interner = UnitInterner::new();
+fn main() -> eyre::Result<()> {
+    color_eyre::install()?;
+    // Shelling out to `cargo` since the cargo crate does not provide what we need
 
-    let bcx = create_bcx(&ws, &compile_opts, &interner, None).context("resolving workspace")?;
+    let unit_graph: UnitGraph = {
+        let sys_args: Vec<_> = std::env::args_os().collect();
+        let output = std::process::Command::new("cargo")
+            .args(&sys_args[1..])
+            .arg("-Z")
+            .arg("unstable-options")
+            .arg("--unit-graph")
+            .stderr(Stdio::inherit())
+            .output()
+            .wrap_err("Executing cargo")?;
 
-    // TODO: improve sorting
-    let units = {
-        let mut known_units = HashSet::with_capacity(bcx.unit_graph.len());
-        let mut units = Vec::with_capacity(bcx.unit_graph.len());
-        for root in &bcx.roots {
-            order_units(&mut known_units, &mut units, &bcx.unit_graph, root);
+        if !output.status.success() {
+            eyre::bail!(
+                "Cargo failed with error {}",
+                output.status.code().unwrap_or(-1)
+            )
         }
-        units
+
+        serde_json::from_slice(&output.stdout)?
     };
 
-    // TODO: persist caches.
-    // May be a good idea to make units for Cargo.lock and persist their outputs
-    // in a registry cache
-    for unit in units {}
+    println!("{:#?}", unit_graph);
 
+    // TODO: parse Cargo.toml and make registry to
     Ok(())
 }
