4c2ea11809d4f789b1fa144b300c0a358e20a887
Artemis Tosini <artemis.tosini@obsidian.systems>
2026-08-18 13:01:28 -0400

    fenix is actually not needed

--- flake.nix
+++ flake.nix
@@ -1,26 +1,16 @@
 {
   inputs = {
     nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs =
     {
       nixpkgs,
-      fenix,
       self,
     }:
     let
       inherit (nixpkgs) lib;
-      makePkgs =
-        system:
-        import nixpkgs {
-          inherit system;
-          overlays = [ fenix.overlays.default ];
-        };
+      makePkgs = system: import nixpkgs { inherit system; };
       forAllSystems = f: lib.genAttrs lib.systems.flakeExposed (system: f system (makePkgs system));
     in
     {
@@ -38,31 +28,32 @@
               rust-analyzer
             ];
 
-            # Use unstable cargo to be safe.
-            # Technically we could use __CARGO_TEST_CHANNEL_OVERRIDE_DO_NOT_USE_THIS=nightly instead
-            CARGO = lib.getExe' pkgs.fenix.complete.cargo "cargo";
-
+            # For some reason nixpkgs cargo will accept unstable features.
+            # If it didn't we could use __CARGO_TEST_CHANNEL_OVERRIDE_DO_NOT_USE_THIS=nightly
+            # or cargo (but not rustc) from fenix
             BUILD_WRAP = lib.getExe self.packages.${system}.build-wrap;
             ENV_WRAP = lib.getExe self.packages.${system}.env-wrap;
           };
         }
       );
 
-      packages = forAllSystems (system: pkgs: {
-        # these two only use stdlib, no need to use cargo
-        build-wrap = pkgs.buildRustCrate {
-          crateName = "build-wrap";
-          version = "0.1.0";
-          src = ./build-wrap;
-          crateBin = [ { name = "build-wrap"; } ];
-        };
-        env-wrap = pkgs.buildRustCrate {
-          crateName = "env-wrap";
-          version = "0.1.0";
-          src = ./env-wrap;
-          crateBin = [ { name = "env-wrap"; } ];
-        };
-      });
+      packages = forAllSystems (
+        system: pkgs: {
+          # these two only use stdlib, no need to use cargo
+          build-wrap = pkgs.buildRustCrate {
+            crateName = "build-wrap";
+            version = "0.1.0";
+            src = ./build-wrap;
+            crateBin = [ { name = "build-wrap"; } ];
+          };
+          env-wrap = pkgs.buildRustCrate {
+            crateName = "env-wrap";
+            version = "0.1.0";
+            src = ./env-wrap;
+            crateBin = [ { name = "env-wrap"; } ];
+          };
+        }
+      );
 
       formatter = forAllSystems (system: pkgs: pkgs.nixfmt-tree);
     };
