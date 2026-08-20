73f8d0723b726769ec69ffa353e0d3ee3563068b
Artemis Tosini <artemis.tosini@obsidian.systems>
2026-08-20 11:16:41 -0400

    nix: use overlays for packages

--- flake.nix
+++ flake.nix
@@ -10,15 +10,42 @@
     }:
     let
       inherit (nixpkgs) lib;
-      makePkgs = system: import nixpkgs { inherit system; };
-      forAllSystems = f: lib.genAttrs lib.systems.flakeExposed (system: f system (makePkgs system));
+      makePkgs =
+        system:
+        import nixpkgs {
+          inherit system;
+          overlays = [ self.overlays.default ];
+        };
+      forAllSystems = f: lib.genAttrs lib.systems.flakeExposed (system: f (makePkgs system));
     in
     {
-      devShells = forAllSystems (
-        system: pkgs: {
-          default = pkgs.mkShell {
-            packages = with pkgs; [
-              # Use nixpkgs rust, fenix rust uses an integrated ld that doesn't set proper rpaths
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            # Use nixpkgs rust, fenix rust uses an integrated ld that doesn't set proper rpaths
+            rustc
+            cargo
+            rustfmt
+            clippy
+
+            gdb
+            rust-analyzer
+          ];
+
+          # For some reason nixpkgs cargo will accept unstable features.
+          # If it didn't we could use __CARGO_TEST_CHANNEL_OVERRIDE_DO_NOT_USE_THIS=nightly
+          # or cargo (but not rustc) from fenix
+          BUILD_WRAP = lib.getExe pkgs.build-wrap;
+          ENV_WRAP = lib.getExe pkgs.env-wrap;
+          TARGET_ENV = lib.getExe pkgs.target-env;
+        };
+        cross =
+          let
+            pkgs' = pkgs.pkgsCross.aarch64-multiplatform;
+          in
+          pkgs'.stdenv.mkDerivation rec {
+            name = "shell-cross";
+            nativeBuildInputs = with pkgs'; [
               rustc
               cargo
               rustfmt
@@ -28,61 +55,37 @@
               rust-analyzer
             ];
 
-            # For some reason nixpkgs cargo will accept unstable features.
-            # If it didn't we could use __CARGO_TEST_CHANNEL_OVERRIDE_DO_NOT_USE_THIS=nightly
-            # or cargo (but not rustc) from fenix
-            BUILD_WRAP = lib.getExe self.packages.${system}.build-wrap;
-            ENV_WRAP = lib.getExe self.packages.${system}.env-wrap;
-            TARGET_ENV = lib.getExe self.packages.${system}.target-env;
+            HOST_CC = lib.getExe pkgs'.buildPackages.stdenv.cc;
+            HOST_CXX = lib.getExe' pkgs'.buildPackages.stdenv.cc "${pkgs'.buildPackages.stdenv.cc.targetPrefix}c++";
+
+            "CARGO_TARGET_${pkgs'.stdenv.buildPlatform.rust.cargoEnvVarTarget}_LINKER" = HOST_CC;
+            "CARGO_TARGET_${pkgs'.hostPlatform.rust.cargoEnvVarTarget}_LINKER" = lib.getExe pkgs'.stdenv.cc;
+
+            BUILD_WRAP = lib.getExe pkgs'.build-wrap;
+            ENV_WRAP = lib.getExe pkgs'.env-wrap;
+            TARGET_ENV = lib.getExe pkgs'.target-env;
           };
-          cross =
-            let
-              pkgs' = pkgs.pkgsCross.aarch64-multiplatform;
-            in
-            pkgs'.stdenv.mkDerivation rec {
-              name = "shell-cross";
-              nativeBuildInputs = with pkgs'; [
-                rustc
-                cargo
-                rustfmt
-                clippy
+      });
 
-                gdb
-                rust-analyzer
-              ];
-
-              HOST_CC = lib.getExe pkgs'.buildPackages.stdenv.cc;
-              HOST_CXX = lib.getExe' pkgs'.buildPackages.stdenv.cc "${pkgs'.buildPackages.stdenv.cc.targetPrefix}c++";
-
-              "CARGO_TARGET_${pkgs'.stdenv.buildPlatform.rust.cargoEnvVarTarget}_LINKER" = HOST_CC;
-              "CARGO_TARGET_${pkgs'.hostPlatform.rust.cargoEnvVarTarget}_LINKER" = lib.getExe pkgs'.stdenv.cc;
-
-              BUILD_WRAP = lib.getExe self.packages.${system}.build-wrap;
-              ENV_WRAP = lib.getExe self.packages.${system}.env-wrap;
-              TARGET_ENV = lib.getExe self.packages.${system}.target-env;
-            };
-        }
-      );
-
-      packages = forAllSystems (
-        system: pkgs:
+      overlays.default =
+        final: prev:
         let
           # these only use stdlib, no need to use cargo
           makeHelper =
             name:
-            pkgs.buildRustCrate {
+            final.buildRustCrate {
               crateName = name;
               version = "0.1.0";
               src = ./${name};
               crateBin = [ { inherit name; } ];
             };
         in
-        rec {
+        {
           build-wrap = makeHelper "build-wrap";
           env-wrap = makeHelper "env-wrap";
           target-env = makeHelper "target-env";
 
-          cargo-dyndrv = pkgs.rustPlatform.buildRustPackage rec {
+          cargo-dyndrv = final.rustPlatform.buildRustPackage rec {
             pname = "cargo-dyndrv";
             version = "0.1.0";
 
@@ -98,20 +101,30 @@
             ];
 
             env = {
-              BUILD_WRAP = lib.getExe build-wrap;
-              ENV_WRAP = lib.getExe env-wrap;
-              TARGET_ENV = lib.getExe target-env;
-              LN = lib.getExe' pkgs.coreutils "ln";
+              BUILD_WRAP = lib.getExe final.build-wrap;
+              ENV_WRAP = lib.getExe final.env-wrap;
+              TARGET_ENV = lib.getExe final.target-env;
+              LN = lib.getExe' final.coreutils "ln";
             };
 
             meta.mainProgram = "cargo-dyndrv";
           };
 
-          writeExtern = pkgs.callPackage ./nix/write-extern.nix { };
-          buildCrate = pkgs.callPackage ./nix/build-crate.nix { inherit writeExtern cargo-dyndrv; };
-        }
-      );
+          writeExtern = final.callPackage ./nix/write-extern.nix { };
+          buildCrate = final.callPackage ./nix/build-crate.nix { };
+        };
 
-      formatter = forAllSystems (system: pkgs: pkgs.nixfmt-tree);
+      packages = forAllSystems (pkgs: {
+        inherit (pkgs)
+          build-wrap
+          env-wrap
+          target-env
+          cargo-dyndrv
+          writeExtern
+          buildCrate
+          ;
+      });
+
+      formatter = forAllSystems (pkgs: pkgs.nixfmt-tree);
     };
 }
